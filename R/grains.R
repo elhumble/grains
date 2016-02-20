56ce08be11d367a99a9f3e8af224143855dcb550
emilyh <emily.lhumble@gmail.com>
2016-02-20 20:06:30 +0000

    read me

--- R/grains.R
+++ R/grains.R
@@ -14,7 +14,7 @@
 #' Calculates how much water you need for your grains. I am working on the grain repetoir and ratios
 #' and accept no responsibility for failed cooking attempts.
 #'
-#' @author  Emily Humble (emily.lhumble@@gmail.com)
+#' @author  Emily (emily.lhumble@@gmail.com)
 #'
 #' @docType package
 #' @name grains
