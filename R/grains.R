29ca1c0117cf2d8f01301ef392784ddb0d0d024b
emilyh <emily.lhumble@gmail.com>
2016-02-22 09:19:07 +0100

    typo

--- R/grains.R
+++ R/grains.R
@@ -11,7 +11,7 @@
 #'
 #' @details
 #'
-#' Calculates how much water you need for your grains. I am working on the grain repetoir and ratios
+#' Calculates how much water you need for your grains. I am working on the grain repetoire and ratios
 #' and accept no responsibility for failed cooking attempts.
 #'
 #' @author  Emily (emily.lhumble@@gmail.com)
